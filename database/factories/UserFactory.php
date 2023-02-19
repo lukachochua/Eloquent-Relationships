60da02c731fdf6799cc13e32dd4900a3ccfdc32a
lukachochua <lukachochua@gmail.com>
2023-02-19 16:47:43 +0400

    Episode 4: has many through

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Affiliation;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -24,6 +25,9 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'experience_points' => 0,
+            'affiliation_id' => function() {
+                return Affiliation::factory()->create()->id;
+            },
         ];
     }
 
--- database/migrations/2023_02_19_122659_create_affiliations_table.php
+++ database/migrations/2023_02_19_122659_create_affiliations_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('affiliations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->unsignedInteger('affiliation_id');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedInteger('experience_points');
-            $table->rememberToken();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('affiliations');
     }
 };
